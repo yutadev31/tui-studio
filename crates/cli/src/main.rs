c5bb334a0dfee1af651459a595712e3b0f6966f8
Yuta <jp.yuta0131@gmail.com>
2025-01-23 06:49:17 +0900

    update

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,16 +1,32 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
 use anyhow::Result;
 use crossterm::event;
 use editor::Editor;
+use utils::event::Event;
 use utils::term::init_term;
+use utils::window::Window;
 
 fn main() -> Result<()> {
     init_term()?;
 
-    let mut editor = Editor::new()?;
-    editor.draw()?;
+    let editor = Arc::new(Mutex::new(Editor::new()?));
+
+    let editor_clone = Arc::clone(&editor);
+    thread::spawn(move || loop {
+        let editor = editor_clone.lock().unwrap();
+        editor.draw().unwrap();
+        thread::sleep(Duration::from_millis(16));
+    });
 
     loop {
-        editor.on_event(event::read()?)?;
-        editor.draw()?;
+        let event = Event::CrosstermEvent(event::read()?);
+        {
+            let mut editor = editor.lock().unwrap();
+            editor.on_event(event)?;
+            editor.draw()?;
+        }
     }
 }
