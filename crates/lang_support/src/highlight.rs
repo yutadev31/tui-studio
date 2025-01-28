692c147843c5bbc472cdc589acabf3935ec85116
Yuta <jp.yuta0131@gmail.com>
2025-01-28 16:05:10 +0900

    update

--- crates/lang_support/src/highlight.rs
+++ crates/lang_support/src/highlight.rs
@@ -1,7 +1,8 @@
+use std::collections::HashSet;
+
 #[derive(Clone)]
 pub struct HighlightToken {
-    pub start: usize,
-    pub end: usize,
+    pub text: String,
     pub kind: TokenKind,
 }
 
@@ -9,9 +10,48 @@ pub struct HighlightToken {
 pub enum TokenKind {
     Keyword,
     Symbol,
+    Bracket,
     String,
     Comment,
     Identifier,
     Number,
     Other,
+}
+
+pub struct SyntaxDefinition {
+    pub keywords: HashSet<String>,
+    pub comments: CommentDefinition,
+    pub brackets: Vec<Pair>,
+    pub string_delimiters: Vec<String>,
+}
+
+pub struct CommentDefinition {
+    pub line: Vec<String>,
+    pub block: Vec<Pair>,
+}
+
+pub struct Pair {
+    pub start: String,
+    pub end: String,
+}
+
+/// トークナイザー
+pub struct Tokenizer<'a> {
+    syntax: &'a SyntaxDefinition,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(syntax: &'a SyntaxDefinition) -> Self {
+        Self { syntax }
+    }
+
+    /// 入力文字列をトークン化
+    pub fn tokenize(&self, source_code: &str) -> Vec<HighlightToken> {
+        let mut tokens = Vec::new();
+        let mut chars = source_code.chars();
+
+        while let Some(c) = chars.next() {}
+
+        tokens
+    }
 }
--- crates/langs/src/html.rs
+++ crates/langs/src/html.rs
@@ -1,34 +1,11 @@
-use lang_support::{highlight::HighlightToken, LanguageSupport};
+use lang_support::LanguageSupport;
 
 use utils::file_type::HTML;
 
-#[derive(Clone)]
 pub struct HTMLLanguageSupport {}
 
 impl HTMLLanguageSupport {
     pub fn new() -> Self {
-        // let syntax = SyntaxDefinition {
-        //     keywords: vec![
-        //         "html".to_string(),
-        //         "head".to_string(),
-        //         "body".to_string(),
-        //         "div".to_string(),
-        //         "p".to_string(),
-        //         "title".to_string(),
-        //     ],
-        //     comment: Comment {
-        //         block: vec![Pair {
-        //             start: "<!--".to_string(),
-        //             end: "-->".to_string(),
-        //         }],
-        //         line: vec![],
-        //     },
-        //     brackets: vec![Pair {
-        //         start: "<".to_string(),
-        //         end: ">".to_string(),
-        //     }],
-        // };
-
         Self {}
     }
 }
@@ -36,9 +13,5 @@ impl HTMLLanguageSupport {
 impl LanguageSupport for HTMLLanguageSupport {
     fn file_type(&self) -> &'static str {
         HTML
-    }
-
-    fn highlight(&self, source_code: &str) -> Option<Vec<HighlightToken>> {
-        None
     }
 }
